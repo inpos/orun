361aa9b93eb02a1468d3bb85af6578ccf3a131d3
inpos <inpos@yandex.ru>
2018-08-08 13:34:18 +0300

    switch to setuptools

--- setup.py
+++ setup.py
@@ -1,13 +1,26 @@
-from distutils.core import setup
+import setuptools
+import os
 
-setup(
+def package_files(directory):
+    paths = []
+    for (path, directories, filenames) in os.walk(directory):
+        for filename in filenames:
+            paths.append(os.path.join('..', path, filename))
+    return paths
+
+extra_files = package_files('examples')
+extra_files.extend(package_files('extjs/static'))
+
+setuptools.setup(
     name='Orun',
     version='0.1.0',
     author='Alexandre L. Dias',
     author_email='alexandre@katrid.com',
-    packages=['orun', 'orun.examples', 'orun.extjs', 'orun.servers'],
+    packages=setuptools.find_packages(),
+    package_data={'': extra_files},
+    include_package_data=True,
     url='http://pypi.python.org/pypi/Orun/',
-    license='LICENSE.txt',
+    license='LICENSE',
     description='Orun (Object RUNtime) Python JavaScript RIA framework.',
     long_description=open('README').read(),
 )
