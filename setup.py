e13109f5aa71a79ab1953740e791e651219939cb
inpos <inpos@yandex.ru>
2018-08-08 11:07:52 +0300

    fix install

--- setup.py
+++ setup.py
@@ -5,9 +5,9 @@ setup(
     version='0.1.0',
     author='Alexandre L. Dias',
     author_email='alexandre@katrid.com',
-    packages=['orun', 'orun.test'],
+    packages=['orun'],
     url='http://pypi.python.org/pypi/Orun/',
     license='LICENSE.txt',
     description='Orun (Object RUNtime) Python JavaScript RIA framework.',
-    long_description=open('README.txt').read(),
+    long_description=open('README').read(),
 )
