29736e5188db9c3493a68be7798f09d7e9cea8f3
inpos <inpos@yandex.ru>
2018-08-09 15:03:12 +0300

    pretty output from convertor

--- orun/js.py
+++ orun/js.py
@@ -19,10 +19,10 @@ class FuncWithParams:
         self.params = params
 
 def list2extjs(l):
-    return '[ %s ]' % ', '.join([encode(v) for v in l])
+    return '[\n%s\n]' % ',\n'.join([encode(v) for v in l])
 
 def dict2extjs(d):
-    return '{ %s }' % ', '.join('%s: %s' % (k, encode(v)) for k,v in d.items())
+    return '{\n%s\n}' % ',\n'.join('%s: %s' % (k, encode(v)) for k,v in d.items())
 
     
 def encode(o):
