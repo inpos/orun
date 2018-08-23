55bcd4d90f7da1f44c75232a390ac84e8ec6b79f
inpos <inpos@yandex.ru>
2018-08-23 15:28:20 +0300

    setuptools fix

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ import os
 
 def package_files(directory):
     paths = []
-    for (path, _, filenames) in os.walk(directory):
+    for (path, _, filenames) in os.walk('orun/' + directory):
         for filename in filenames:
-            paths.append(os.path.join('..', path, filename))
+            paths.append(os.path.join(path, filename))
     return paths
 
 extra_files = package_files('examples')
 extra_files.extend(package_files(os.path.join('extjs', 'static')))
-extra_files.append(os.path.join('extjs', 'app.html'))
+extra_files.append(os.path.join('orun', 'extjs', 'app.html'))
 
 
 setuptools.setup(
@@ -20,6 +20,7 @@ setuptools.setup(
     author_email='alexandre@katrid.com',
     packages=setuptools.find_packages(),
     package_data={'': extra_files},
+    data_files=[('',extra_files)],
     include_package_data=True,
     zip_safe=False,
     url='http://pypi.python.org/pypi/Orun/',
