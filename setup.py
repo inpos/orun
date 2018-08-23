f465f2e42653f48360b556a8633f1ba7eb9e7f9d
inpos <inpos@yandex.ru>
2018-08-23 18:48:14 +0300

    ...

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setuptools.setup(
     version='0.4.0',
     author='Alexandre L. Dias, Roman Borodin (inpos@yandex.ru)',
     author_email='alexandre@katrid.com',
+    packages=setuptools.find_packages(),
     include_package_data=True,
     zip_safe=False,
     url='http://pypi.python.org/pypi/Orun/',
